b5bd456006afe74104ae6584be673aa10fda3cdc
OgurchickRick <artem.derbin.04@list.ru>
2022-10-29 22:56:40 +0300

    Создание БД

---
+++ src/Connect.java
@@ -0,0 +1,65 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Connect {
+    public static Connection conn;
+    public static Statement statmt;
+    public static ResultSet resSet;
+
+    // --------ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ--------
+    public static void Conn() throws ClassNotFoundException, SQLException
+    {
+        conn = null;
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:My_cats.s3db");
+
+        System.out.println("CONNECT!");
+    }
+
+    // --------Создание таблицы--------
+    public static void CreateDB() throws ClassNotFoundException, SQLException
+    {
+        statmt = conn.createStatement();
+        statmt.execute("CREATE TABLE if not exists 'types' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'type' text);");
+
+        System.out.println("TABLE CREATE!");
+    }
+
+    // --------Заполнение таблицы--------
+    public static void WriteDB() throws SQLException
+    {
+        System.out.println("TABLE WRITE");
+    }
+
+    // -------- Вывод таблицы--------
+    public static void ReadDB() throws ClassNotFoundException, SQLException
+    {
+        resSet = statmt.executeQuery("SELECT * FROM types");
+
+        while(resSet.next())
+        {
+            int id = resSet.getInt("id");
+            String type = resSet.getString("type");
+            System.out.println( "ID = " + id );
+            System.out.println( "type = " + type );
+            System.out.println();
+        }
+
+        System.out.println("TABLE INPUT");
+    }
+
+    // --------Закрытие--------
+    public static void CloseDB() throws ClassNotFoundException, SQLException
+    {
+        conn.close();
+        statmt.close();
+        resSet.close();
+
+        System.out.println("CONNECT CLOSE");
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)throws ClassNotFoundException, SQLException {
+        Connect.Conn();
+        Connect.CreateDB();
+        Connect.WriteDB();
+        Connect.ReadDB();
+        Connect.CloseDB();
     }
 }
